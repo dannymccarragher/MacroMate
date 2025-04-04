3f152ceb9567ccdbf88f4e51bf2c6023bc22e426
Danny McCarragher <dannymccarragher@gmail.com>
2025-04-04 10:22:04 -0700

    Config cors on backend

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import router from './router/router.js';
+import cors from 'cors';
+
+const app = express();
+const PORT = 3000;
+
+app.set("view engine", "pug"); 
+app.set('views', "frontend/views")
+app.use(express.static("public"));
+
+app.use(cors());
+app.use(express.urlencoded({ extended : true}));
+app.use(express.json());
+
+app.use('/', router);
+
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
