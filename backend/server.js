ae8a824fdbbc9962701e3785f3c608a14a022a0b
dannymccarragher <dannymccarragher@gmail.com>
2025-10-17 22:16:00 -0700

    remove logs

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,7 @@ app.set("view engine", "pug");
 app.set('views', "frontend/views")
 app.use(express.static("public"));
 
-// Enable CORS for all routes - more permissive for development
 app.use(cors());
-
-// Handle preflight requests
-app.options('*', cors());
 
 app.use(express.urlencoded({ extended : true}));
 app.use(express.json());
